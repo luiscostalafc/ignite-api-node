ad764bcd9576d8d23525168633fec708f93348c5
luiscostalafc <luiscostalafc@gmail.com>
2021-04-19 22:56:10 -0300

    feat: create categories

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import express from 'express';
+import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
+app.use(express.json());
 
-app.listen(3333);
+app.use("/categories", categoriesRoutes);
+
+app.listen(3333, () => console.log("Server is running! 🚀"));
