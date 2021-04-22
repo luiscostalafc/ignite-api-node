172942a81d73da1ab37396279b338e9cc94a55b9
luiscostalafc <luiscostalafc@gmail.com>
2021-04-21 21:10:30 -0300

    fix: refactor specifications

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,12 +1,12 @@
-import { ISpecificationRepository } from "../repositories/ISpecificationsRepository";
+import { ISpecificationRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateSpecificationService {
-  constructor(private specificationsRepository: ISpecificationRepository) { }
+class CreateSpecificationUseCase {
+  constructor(private specificationsRepository: ISpecificationRepository) {}
   execute({ name, description }: IRequest): void {
     const specificationAlreadyExists = this.specificationsRepository.findByName(
       name
@@ -23,4 +23,4 @@ class CreateSpecificationService {
   }
 }
 
-export { CreateSpecificationService };
+export { CreateSpecificationUseCase };
