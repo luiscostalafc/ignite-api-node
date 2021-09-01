9e06846e7e5c931ff8bd400a5e620820cbe23ef5
luiscostalafc <Nanico2406>
2021-09-01 20:52:12 -0300

    fix: solve probleman in the Car modal

---
+++ src/modules/cars/useCases/createCars/CreateCarController.ts
@@ -0,0 +1,34 @@
+import { Response, Request } from "express";
+import { container } from "tsyringe";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+class CreateCarController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const {
+      name,
+      description,
+      daily_rate,
+      brand,
+      category_id,
+      fine_amount,
+      license_plate,
+    } = request.body;
+
+    const createCarUseCase = container.resolve(CreateCarUseCase);
+
+    const car = await createCarUseCase.execute({
+      name,
+      description,
+      daily_rate,
+      brand,
+      category_id,
+      fine_amount,
+      license_plate,
+    });
+
+    return response.status(201).json(car);
+  }
+}
+
+export { CreateCarController };
